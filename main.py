6db1349e8dd32a25e4ac1fd7e852a93c93623c49
JarrettR <jrainier@gmail.com>
2023-02-14 00:21:42 -0800

    Layout tweaking

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def move():
     
     window.after(33, move)
 
+def reset():
+    print("Reset")
+    
 window = tk.Tk()
 
 # window.geometry("600x400")
@@ -13,11 +16,22 @@ window = tk.Tk()
 c = tk.Canvas(window, height=400,width=600)
 c.pack(fill=tk.Y, side=tk.TOP)
 
-frame3 = tk.Frame(master=window, height=50, bg="blue")
-frame3.pack(fill=tk.X, side=tk.BOTTOM)
+frame = tk.Frame(master=window, height=50,relief=tk.RAISED,borderwidth=1)
+frame.pack(fill=tk.X, side=tk.BOTTOM)
+
+frame.rowconfigure(0, minsize=50, weight=1)
+
+frame.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+label = tk.Label(text="Name", master=frame)
+label.grid(row=0, column=0, sticky="nsew")
+entry = tk.Entry(master=frame)
+entry.grid(row=0, column=1)
+
+button = tk.Button(text="Reset", master=frame, command=reset)
+button.grid(row=0, column=2, sticky="nsew")
 
 circle = c.create_oval(60,60,210,210)
-
 move()
 
 window.mainloop()
