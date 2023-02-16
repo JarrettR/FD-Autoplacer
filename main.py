7a69acc6923eb2f0eb204b13cdf3a347933d598f
JarrettR <jrainier@gmail.com>
2023-02-15 17:30:16 -0800

    Got option box displaying nets

--- main.py
+++ main.py
@@ -30,33 +30,43 @@ class Footprint:
         c.move(circle, 5, 5)
         self.coord = c.coords(circle)
 
+
 if __name__ == '__main__':
     window = tk.Tk()
 
     # window.geometry("600x400")
 
-    c = tk.Canvas(window, height=400,width=600)
-    c.pack(fill=tk.Y, side=tk.TOP)
 
-    frame = tk.Frame(master=window, height=50,relief=tk.RAISED,borderwidth=1)
-    frame.pack(fill=tk.X, side=tk.BOTTOM)
+    optFrame = tk.Frame(master=window, height=50,relief=tk.RAISED,borderwidth=1)
+    optFrame.pack(fill=tk.X, side=tk.BOTTOM)
+    netFrame = tk.Frame(master=window, width=250,relief=tk.SUNKEN,borderwidth=1)
+    netFrame.pack(fill=tk.Y, side=tk.RIGHT)
+    c = tk.Canvas(window, height=600,width=600)
+    c.pack(fill=tk.Y, side=tk.LEFT)
 
-    frame.rowconfigure(0, minsize=50, weight=1)
 
-    frame.columnconfigure([0, 1, 2], minsize=50, weight=1)
+    optFrame.rowconfigure(0, minsize=50, weight=1)
 
-    label = tk.Label(text="Name", master=frame)
+    optFrame.columnconfigure([0, 1, 2], minsize=50, weight=1)
+
+    label = tk.Label(text="Name", master=optFrame)
     label.grid(row=0, column=0, sticky="nsew")
-    entry = tk.Entry(master=frame)
+    entry = tk.Entry(master=optFrame)
     entry.grid(row=0, column=1)
 
-    button = tk.Button(text="Reset", master=frame, command=reset)
+    button = tk.Button(text="Reset", master=optFrame, command=reset)
     button.grid(row=0, column=2, sticky="nsew")
 
     circle = c.create_oval(60,60,210,210)
-    what = Board()
-    what.Load()
-    print(what.net)
+    pcb = Board()
+    pcb.Load()
+
+    #net #, name, count, checked
+    for i, net in enumerate(pcb.net):
+        if net[2] > 1:
+            pcb.net[i].append(tk.IntVar(value=1))
+            netlabel = tk.Checkbutton(text=net[1], master=netFrame, justify=tk.LEFT, variable=pcb.net[i][3])
+            netlabel.pack(fill=tk.X, side=tk.TOP)
     
     Footprint(window,c)
     move()
