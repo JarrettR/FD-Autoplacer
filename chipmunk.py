d59fa01f736421e59e265d1b2d5871c2bdeb8650
JarrettR <jrainier@gmail.com>
2024-03-27 10:50:01 -0700

    Added pymunk example

---
+++ chipmunk.py
@@ -0,0 +1,124 @@
+
+import math
+import random
+import sys
+
+import pygame
+
+import pymunk
+import pymunk.pygame_util
+from pymunk import Vec2d
+
+random.seed(5)
+
+pymunk.pygame_util.positive_y_is_up = True
+
+
+def draw_collision(arbiter, space, data):
+    for c in arbiter.contact_point_set.points:
+        r = max(3, abs(c.distance * 5))
+        r = int(r)
+
+        p = pymunk.pygame_util.to_pygame(c.point_a, data["surface"])
+        pygame.draw.circle(data["surface"], pygame.Color("black"), p, r, 1)
+
+gravityStrength = 5.0e6
+
+
+def planetGravity(body, gravity, damping, dt):
+    # Gravitational acceleration is proportional to the inverse square of
+    # distance, and directed toward the origin. The central planet is assumed
+    # to be massive enough that it affects the satellites but not vice versa.
+    sq_dist = body.position.get_dist_sqrd((300, 300))
+    g = (
+        (body.position - pymunk.Vec2d(300, 300))
+        * -gravityStrength
+        / (sq_dist * math.sqrt(sq_dist))
+    )
+    pymunk.Body.update_velocity(body, g, damping, dt)
+    
+    
+def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()
+    running = True
+
+    ### Physics stuff
+    space = pymunk.Space()
+    # space.gravity = (0.0, -900.0)
+    draw_options = pymunk.pygame_util.DrawOptions(screen)
+    # disable the build in debug draw of collision point since we use our own code.
+    draw_options.flags = (
+        draw_options.flags ^ pymunk.pygame_util.DrawOptions.DRAW_COLLISION_POINTS
+    )
+    ## Balls
+    balls = []
+
+    ### walls
+    static_lines = [
+        pymunk.Segment(space.static_body, (11.0, 280.0), (407.0, 246.0), 0.0),
+        pymunk.Segment(space.static_body, (407.0, 246.0), (407.0, 343.0), 0.0),
+    ]
+    for l in static_lines:
+        l.friction = 0.5
+    space.add(*static_lines)
+
+    ticks_to_next_ball = 10
+
+    ch = space.add_collision_handler(0, 0)
+    ch.data["surface"] = screen
+    ch.post_solve = draw_collision
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                pygame.image.save(screen, "contact_with_friction.png")
+
+        ticks_to_next_ball -= 1
+        if ticks_to_next_ball <= 0:
+            ticks_to_next_ball = 100
+            mass = 0.1
+            radius = 25
+            inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
+            body = pymunk.Body(mass, inertia)
+            x = random.randint(115, 350)
+            body.position = x, 400
+            # body.velocity_func = planetGravity
+            shape = pymunk.Circle(body, radius, (0, 0))
+            shape.friction = 0.5
+            space.add(body, shape)
+            balls.append(shape)
+
+        ### Clear screen
+        screen.fill(pygame.Color("white"))
+
+        ### Draw stuff
+        space.debug_draw(draw_options)
+
+        balls_to_remove = []
+        for ball in balls:
+            if ball.body.position.y < 200:
+                balls_to_remove.append(ball)
+        for ball in balls_to_remove:
+            space.remove(ball, ball.body)
+            balls.remove(ball)
+
+        ### Update physics
+        dt = 1.0 / 60.0
+        for x in range(1):
+            space.step(dt)
+
+        ### Flip screen
+        pygame.display.flip()
+        clock.tick(50)
+        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
